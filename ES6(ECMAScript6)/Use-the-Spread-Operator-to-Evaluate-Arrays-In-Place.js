9ce3e6db8fce83ff5e678f3bfb9c67191213919d
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-12-06 13:16:49 -0500

    Update Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js

--- ES6(ECMAScript6)/Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
+++ ES6(ECMAScript6)/Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -13,10 +13,5 @@ console.log(arr2);
   
   
 //solution
-const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
-let arr2;
 
-arr2 = [...arr1];  // Change this line
-
-console.log(arr2);
-
+arr2 = [...arr1]; 
