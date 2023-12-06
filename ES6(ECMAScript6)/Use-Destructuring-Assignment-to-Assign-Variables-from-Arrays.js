8dbd686e02a413952636c9d8f81cd3211ca394c4
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-12-06 13:07:47 -0500

    Create Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js

---
+++ ES6(ECMAScript6)/Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,10 @@
+
+//problem
+Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
+
+
+//solution
+let a = 8, b = 6;
+// Only change code below this line
+
+[a, b] = [b, a];
