bd37c45b938d7c63226f5c85fa3604f0a2fdd290
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-12-06 12:57:15 -0500

    Create Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js

---
+++ ES6(ECMAScript6)/Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,22 @@
+
+//problem
+Copy all contents of arr1 into another array arr2 using the spread operator.
+
+  
+//orginal look
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [];  // Change this line
+
+console.log(arr2);
+  
+  
+//solution
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+
