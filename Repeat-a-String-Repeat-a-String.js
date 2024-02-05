662e3812dc3dcd1bca0508d1219e6a64830d1b05
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-02-05 13:44:07 -0500

    Create Repeat-a-String-Repeat-a-String.js

---
+++ Repeat-a-String-Repeat-a-String.js
@@ -0,0 +1,29 @@
+//problem 
+Repeat a given string str (first argument) for num times (second argument). 
+Return an empty string if num is not a positive number. 
+For the purpose of this challenge, do not use the built-in .repeat() method.
+  
+//solution 1
+function repeatStringNumTimes(str, num) {
+  let accumulatedStr = "";
+
+  for (let i = 0; i < num; i++) {
+    accumulatedStr += str;
+  }
+
+  return accumulatedStr;
+}
+
+//solution 2
+function repeatStringNumTimes(str, num) {
+  if (num < 1) {
+    return "";
+  } else {
+    return str + repeatStringNumTimes(str, num - 1);
+  }
+}
+
+//solution 3
+function repeatStringNumTimes(str, num) {
+  return num > 0 ? str + repeatStringNumTimes(str, num - 1) : '';
+}
